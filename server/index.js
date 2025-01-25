a206b1d2f0ad8f9791ef2390304afff305c74afd
eli.p <eli.pentser@gmail.com>
2025-01-25 09:39:21 +0200

    add env file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log("hello!!!");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+   console.log("DB connection successful!!!")
+})
+.catch((err)=>{
+   console.log(err)
+})
+
+const PORT= process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`erver is running on port: ${PORT}`);
+})
+
