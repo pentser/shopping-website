38f6d0c5936ee4dc1bcfd20c0378028d5da7e1cc
eli.p <eli.pentser@gmail.com>
2025-01-25 14:47:00 +0200

    creates model and bgin with routes

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.js"
 
 dotenv.config();
 const app = express();
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
-   console.log("DB connection successful!!!")
+   console.log("DB connection successful!!")
 })
 .catch((err)=>{
    console.log(err)
 })
 
-const PORT= process.env.PORT;
+app.use(express.json())
+app.use("/api/user",userRoute);
+const PORT= process.env.PORT|| 3001;
 app.listen(PORT,()=>{
     console.log(`erver is running on port: ${PORT}`);
 })
---
+++ server/models/Product.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    title: {type:String,required:true,unique:true},
+    desc: {type:String,required:true}, 
+    img: {type:String,required:true}, 
+    categorys: {type:Array},
+    price: {type:Number,required:true}
+  }
+,
+{timestamps:true});
+
+export default mongoose.model("Product",ProductSchema);
