0179511ed8c45767d23682d26e8804d8e904e26b
eli.p <eli.pentser@gmail.com>
2025-01-26 13:34:28 +0200

    update auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User.js";
 import CryptpJS from "crypto-js";
+import jwt from "jsonwebtoken";
 
 const router= express.Router();
 
@@ -21,6 +22,44 @@ router.post("/register",async (req,res)=>{
     }
 
 })
+
+// LOGIN
+
+router.post("/login",async (req,res)=>{
+
+    try {
+
+        const user= await User.findOne({
+            username: req.body.username
+        })
+        !user && res.status(401).json("Worng credentials");
+    
+        const hashedPassword= CryptpJS.AES.decrypt(user.password,process.env.PASS_SECRET);
+        const passwordS = hashedPassword.toString(CryptpJS.enc.Utf8);
+    
+        passwordS!==req.body.password &&  res.status(401).json("Worng credentials");
+
+        const accessToken =jwt.sign({
+            id:user.id,
+            isAdmin:user.isAdmin
+        },process.env.JWT_SECRET,{expiresIn:"3d"});
+    
+        const {password, ...others} = user._doc;
+        res.status(200).json({others,accessToken});
+    } catch (err){
+
+        res.status(500).json(err);
+
+    }
+
+
+
+
+  
+   
+
+});
+
 
 export default router;
 
